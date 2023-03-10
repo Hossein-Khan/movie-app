05cedcd6438e83ce24c05a1542dbad12ae9eb5dd
Hossein Khanmirzaei <Khanmirzaei.hossein@gmail.com>
2023-03-10 23:55:50 +0330

    Fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
-import MovieModel from "./models/MovieModel";
+import MovieModel, { FetchedMovieList } from "./models/MovieModel";
 
 function App() {
-  const dummyMovies: MovieModel[] = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
+  const [movies, setMovies] = useState<MovieModel[]>([]);
+  const [hasError, setHasError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const fetchMoviesHandler = useCallback(async function () {
+    try {
+      setHasError(null);
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      if (response.status !== 200) {
+        throw new Error("Somthing went wrong!");
+      }
+
+      const data: FetchedMovieList = await response.json();
+      //if(response.status)
+      const receivedMovies = data.results.map(function (movie): MovieModel {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          openingText: movie.opening_crawl,
+        };
+      });
+      setMovies(receivedMovies);
+    } catch (error: unknown) {
+      setHasError((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = (
+    <React.Fragment>
+      <p>There is nothing to show.</p>
+      <p>Try fetching movies.</p>
+    </React.Fragment>
+  );
+  if (movies.length > 0) content = <MoviesList movies={movies} />;
+  if (hasError) content = <p>{hasError}</p>;
+  if (isLoading) content = <p>Loading data...</p>;
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
--- src/models/MovieModel.ts
+++ src/models/MovieModel.ts
@@ -1,3 +1,25 @@
+export interface FetchedMovieList {
+  count: number;
+  next: null;
+  previous: null;
+  results: {
+    characters: string[];
+    created: string;
+    director: string;
+    edited: string;
+    episode_id: number;
+    opening_crawl: string;
+    planets: string[];
+    producer: string;
+    release_date: string;
+    species: string[];
+    starships: string[];
+    title: string;
+    url: string;
+    vehicles: string[];
+  }[];
+}
+
 export default interface MovieModel {
   id: number;
   title: string;
